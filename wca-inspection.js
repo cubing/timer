4eb4ce4eaae71d4096ff1b529de63cc0f76fe23f
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2014-08-02 09:26:31 +0200

    Makes background green if stopped at 7.x even though it's fading into yellow while running.

--- wca-inspection.js
+++ wca-inspection.js
@@ -6,12 +6,13 @@ document.ontouchmove = function (event) {
 var state = "set";
 
 var states = {
-  "none":  {"function": null,       "background": "white",     "down": "ready", "up": "ready"},
-  "ready": {"function": null,       "background": null,        "down": "set",   "up": "ready"},
-  "set":   {"function": set,        "background": "#987",      "down": "set",   "up": "go"   },
-  "go":    {"function": startTimer, "background": "green",     "down": "stop",  "up": "stop" },
-  "stop":  {"function": stopTimer,  "background": null,        "down": "stop", "up": "ready" }
+  "none":  {"function": null,       "background": ["white", "white", "white", "white", "white"],   "down": "ready", "up": "ready"},
+  "ready": {"function": null,       "background": [null, null, null, null, null],                  "down": "set",   "up": "ready"},
+  "set":   {"function": set,        "background": ["#987", "#987", "#987", "#987", "#987"],        "down": "set",   "up": "go"   },
+  "go":    {"function": startTimer, "background": ["green", "green", "green", "green", "green"],   "down": "stop",  "up": "stop" },
+  "stop":  {"function": stopTimer,  "background": ["0f0", "#ff0", "#f80", "#f00", "#800"],         "down": "stop",  "up": "ready" }
 }
+console.log(states["stop"].background[2]);
 
 function set() {
   $("#sec").html("0");
@@ -21,10 +22,12 @@ function set() {
 var startTime;
 var lastTime;
 var counting = false;
+var penalty = 0;
 
 
 function startTimer() {
   counting = true;
+  penalty = 0;
   lastTime = startTime = Date.now();
   animFrame();
 }
@@ -44,17 +47,29 @@ function animFrame() {
     if (lastTime < 7 && time === 7) {
       $("#main").animate({"background-color": "#ff0"}, 1000);
     }
+    if (lastTime < 8 && time === 8) {
+      penalty = 1;
+    }
 
     if (lastTime < 11 && time === 11) {
       $("#main").animate({"background-color": "#f80"}, 1000);
+    }
+    if (lastTime < 12 && time === 12) {
+      penalty = 2;
     }
 
     if (lastTime < 14 && time === 14) {
       $("#main").animate({"background-color": "#f00"}, 1000);
     }
+    if (lastTime < 15 && time === 15) {
+      penalty = 3;
+    }
 
     if (lastTime < 16 && time === 16) {
       $("#main").animate({"background-color": "#800"}, 1000);
+    }
+    if (lastTime < 17 && time === 17) {
+      penalty = 4;
     }
 
     lastTime = time;
@@ -72,7 +87,7 @@ function trigger(dir, event) {
   }
   console.log("state", state);
   if (states[state].background) {
-    $("#main").css("background", states[state].background);
+    $("#main").css("background", states[state].background[penalty]);
   }
   if (states[state]["function"]) {
     (states[state]["function"])();
