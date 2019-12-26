0bcf9cca68afe665e99ddd2337f3c3be296a35f9
Lucas Garron <code@garron.net>
2019-12-26 03:27:07 -0800

    Add Square-1 scrambles.

--- src/scramble/worker-implementation.ts
+++ src/scramble/worker-implementation.ts
@@ -3,6 +3,7 @@ import "babel-polyfill"; // Prevent `regeneratorRuntime is not defined` error. h
 import { expose } from "comlink"
 import { getRandomScramble as getRandomScramble333 } from "../vendor/cstimer/src/js/scramble/scramble_333_edit";
 import { getRandomScramble as getRandomScramble444 } from "../vendor/cstimer/src/js/scramble/scramble_444";
+import { getRandomScramble as getRandomScrambleSq1 } from "../vendor/cstimer/src/js/scramble/scramble_sq1_new";
 import { EventName } from "../cubing";
 
 export interface ScrambleWorker {
@@ -20,6 +21,8 @@ class ScrambleWorkerImpl implements ScrambleWorker {
         return getRandomScramble333();
       case "444":
         return getRandomScramble444();
+      case "sq1":
+        return getRandomScrambleSq1();
       default:
         return "<scramble unavailable>";
     }
