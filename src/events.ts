a203ad63ddbc5a907c53a22b50fc57e6511fa01c
Lucas Garron <code@garron.net>
2022-10-28 18:08:04 -0700

    Update event order to match WCA.

--- src/events.ts
+++ src/events.ts
@@ -7,16 +7,19 @@ export type EventID = keyof typeof puzzles;
 // // Matches the order on the WCA website.
 export const eventOrder: EventID[] = [
   "333",
-  "444",
-  // "555",
   "222",
+  "444",
+  "555",
+  "666",
+  "777",
   "333bf",
   "333oh",
+  "clock",
+  "minx",
   // "333fm", "333ft", "minx",
   "pyram",
-  "sq1",
-  "clock",
   "skewb",
+  "sq1",
   // "666", "777", "444bf", "555bf", "333mbf"
   "fto",
   "master_tetraminx",
