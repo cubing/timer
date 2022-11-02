811971691c38a0adb49e15d339421cbe19b3d5d3
Lucas Garron <code@garron.net>
2022-11-01 18:42:23 -0700

    Change dev port to `3334`.

--- script/dev.js
+++ script/dev.js
@@ -3,6 +3,7 @@ import { barelyServe } from "barely-a-dev-server";
 barelyServe({
   entryRoot: "src/timer.cubing.net",
   devDomain: "timer.localhost",
+  port: 3334,
   esbuildOptions: {
     external: ["crypto"],
     loader: { ".svg": "copy", ".ico": "copy" },
