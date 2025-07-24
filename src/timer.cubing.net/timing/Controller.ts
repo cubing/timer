1055536d24803ec6b4a11ef37eb94087e8675921
Lucas Garron <code@garron.net>
2025-07-24 07:47:27 -0700

    Adjust wording of a comment for more clarity.

--- src/timer.cubing.net/timing/Controller.ts
+++ src/timer.cubing.net/timing/Controller.ts
@@ -16,7 +16,7 @@ type TransitionMap = Record<State, State>;
 
 export class Controller {
   private timer: Timer;
-  // @ts-ignore: This is initialized but TypeScript is not powerful enough to tell.
+  // @ts-ignore: This is initialized unconditionally by the constructor but TypeScript is not powerful enough to tell.
   private state: State;
   constructor(
     private domElement: HTMLElement,
